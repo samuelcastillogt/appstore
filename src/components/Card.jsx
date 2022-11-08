385e82f5ece9c879939218a045adf1905a970280
Samuel Castillo <samuelcastilloseo@gmail.com>
2022-11-08 16:40:42 -0600

    Proyectos a un 80%, solo falta el sistema de estrellas y areegalar el modal

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 const Card = (props) => {
   const handleData = ()=>{
     props.setModal(props.data)
-    document.style.background ="red"
   }
   return (
-        <div className="card" style={{width: "150px", margin: "10px"}} onClick={handleData}>
-  <img src={props.data.img} className="" alt="..." style={{padding: "10px"}}/>
-    <div className="card-content">
-      <div className="card-content-text">
-              <p className="fw-bold">{props.data.name}</p>
-      <p className="fs-6">{props.data.developer}</p>
-      </div>
-
+  <div className="card" style={{width: "150px", margin: "10px"}} onClick={handleData}>
+    <img src={props.data.img} className="" alt="..." style={{padding: "10px"}}/>
+      <div className="card-content">
+        <div className="card-content-text">
+          <p className="fw-bolder">{props.data.name}</p>
+          <p className="fs-6">{props.data.developer}</p>
+        </div>
+        <FontAwesomeIcon icon="fa-regular fa-star" />
+        <i class="fa-regular fa-star"></i>
       <p className='h5'>{props.data.price <= 0.5 ? "Free" : `$.${props.data.price}`}</p>
-    </div>
-    
+      </div>
 </div>
     );
 };
